5d6ae8a2fa30c86c86ae738609e934b9e4b890bd
EdgarAbad <edgarabad20487@iesjoanramis.org>
2023-06-13 19:40:11 +0200

    JS continuat

--- script.js
+++ script.js
@@ -8,4 +8,34 @@ const peliculaSelect = document.getElementById('pelicula');
 
 let preuDelTicket = +peliculaSelect.value;
 
-//console.log(preuDelTicket);
+function actualitzaSeleccioSeients() {
+    
+    const seientsSeleccionats = document.querySelectorAll('.fila .seient.seleccionat');
+    //console.log(seientsSeleccionats);
+
+    const seientsIndex = [...seientsSeleccionats].map((seient) => [...seients].indexOf(seient));
+
+    localStorage.setItem('seientsSeleccionats', JSON.stringify(seientsIndex));
+
+    const contadorSeientsSeleccionats = seientsSeleccionats.length;
+
+    contador.innerText = contadorSeientsSeleccionats;
+    total.innerText = preuDelTicket * contadorSeientsSeleccionats;
+}
+
+contenidor.addEventListener('click', (e) => {
+
+    if(e.target.classList.contains('seient')
+    && !e.target.classList.contains('ocupat')) {
+       
+       
+        e.target.classList.toggle('seleccionat');
+        actualitzaSeleccioSeients();
+    }
+});
+
+peliculaSelect.addEventListener('change', (e) => {
+    preuDelTicket = +e.target.value;
+
+    actualitzaSeleccioSeients();
+});
